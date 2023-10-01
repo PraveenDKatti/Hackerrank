e3c590b87cb71360e6204baa15a10b810e91b369
PraveenDKatti <145376689+PraveenDKatti@users.noreply.github.com>
2023-10-01 15:11:51 +0530

    Create Java Stdin and Stdout 2.java

---
+++ Java/Introduction/Java Stdin and Stdout 2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        double d = s.nextDouble();
+        String st1 = s.nextLine();
+        String st2 = s.nextLine();
+        
+        System.out.println("String: "+st2);
+        System.out.println("Double: "+d);
+        System.out.println("Int: "+a);
+    }
+}
