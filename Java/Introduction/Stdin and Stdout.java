99eea0c2e58ca1339cc51b6058cbb58b2df7ca27
PraveenDKatti <145376689+PraveenDKatti@users.noreply.github.com>
2023-10-01 15:09:10 +0530

    Update Stdin and Stdout.java

--- Java/Introduction/Stdin and Stdout.java
+++ Java/Introduction/Stdin and Stdout.java
@@ -1,1 +1,16 @@
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s = new Scanner(System.in);
+        int a= s.nextInt();
+        int b= s.nextInt();
+        int c= s.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
